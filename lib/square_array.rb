bffb92be86bcf8496efca71f02b3cbb1d7318dd9
Justin Webb <webb.n.justin@gmail.com>
2020-07-15 22:11:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,14 @@
-require "pry"
+
 
 def square_array(numbers)
   count = 0 
   new_array = [] 
   
   while count < numbers.length do  
-    binding.pry
-    numbers[count] **2
+
+    new_array.push(numbers[count] **2) 
     count +=1
-  puts new_array
+  
   end
+  new_array
 end
